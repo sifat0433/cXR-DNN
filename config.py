42c5606ee1f60820db4f753bef63b9843f8cd3ce
Sifat Rezwan <105652970+sifat0433@users.noreply.github.com>
2026-07-08 12:29:37 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,24 +4,24 @@ import torch
 
 @dataclass
 class Config:
-    data_path: str = "/mnt/cluster/workspaces/lichenyan/cXR-DNN/data/ply"
-    data_split_path: str = "/mnt/cluster/workspaces/lichenyan/cXR-DNN/data/split"
+    data_path: str = "/home/gainpc2/Documents/pycharm5090/Gain/ply/cXR-DNN-main/all" 
+    data_split_path: str = "/home/gainpc2/Documents/pycharm5090/Gain/ply/cXR-DNN-main/split"
     grid_size: int = 64
     n_points: int = 4096
 
     num_workers: int = 4
     batch_size: int = 16
-    batch_size_val_test: int = 256
+    batch_size_val_test: int = 1
     batch_size_ann2snn: int = 32
-    batch_size_ann2snn_test: int = 64
+    batch_size_ann2snn_test: int = 1
     batch_size_snn_train: int = 4
     device: str = "cuda" if torch.cuda.is_available() else "cpu"
 
-    snn_steps: int = 64
+    snn_steps: int = 9
 
     latent_dim: int = 128
     
-    epochs: int = 100
+    epochs: int = 1
     lr: float = 1e-3
 
     dice_weight: float = 0.7
